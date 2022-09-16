13c8a767a7d12a4b907b6997fb34e979cf9f47da
picccard <eskil.uhlving@gmail.com>
2022-09-16 02:13:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,17 @@ app.get('/', (req, res) => {
 
 app.get('/site', (req, res) => {
     res.status(200).json(req.headers)
-  })
+})
+
+app.get('/env', (req, res) => {
+    const host = process.env.MY_NAME
+    res.status(200).json({
+      'sitename': process.env.WEBSITE_SITE_NAME || 'sitename not found',
+      'resourcegroup': process.env.WEBSITE_RESOURCE_GROUP || 'resourcegroup not found',
+      'hostname': process.env.WEBSITE_HOSTNAME || 'hostname not found',
+    })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
