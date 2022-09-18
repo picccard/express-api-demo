b5f89320228753708e8c0c9a245c88d6ed043ca5
picccard <eskil.uhlving@gmail.com>
2022-09-18 18:02:39 +0200

    add /health endpoint

--- server.js
+++ server.js
@@ -10,13 +10,23 @@ app.get('/site', (req, res) => {
     res.status(200).json(req.headers)
 })
 
+app.get('/health', (req, res) => {
+    res.status(200).json("Healthy")
+})
+
+app.get('/region', (req, res) => {
+    res.status(200).json({
+        'regionPicccard': process.env.PICCCARD_RANDOM || 'env variable not found'
+    })
+})
+
 app.get('/env', (req, res) => {
     const host = process.env.MY_NAME
     res.status(200).json({
       'sitename': process.env.WEBSITE_SITE_NAME || 'sitename not found',
       'resourcegroup': process.env.WEBSITE_RESOURCE_GROUP || 'resourcegroup not found',
       'hostname': process.env.WEBSITE_HOSTNAME || 'hostname not found',
-      'picccardrandom': process.env.PICCCARD_RANDOM || 'hostname not found',
+      'picccardrandom': process.env.PICCCARD_RANDOM || 'env varaible PICCCARD_RANDOM not found',
     })
 })
 
